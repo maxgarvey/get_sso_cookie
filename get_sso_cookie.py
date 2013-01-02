d0003b03d63596057b3ddc5a54fb89d51d101473
maxgarvey <maxgarvey@gmail.com>
2013-01-02 12:22:32 -0800

    added cookies.json to gitignore and added docstring to src.py

--- get_sso_cookie.py
+++ get_sso_cookie.py
@@ -3,6 +3,15 @@ import json
 
 def get_cookies(username, password, outfile="cookies.json",
                    url="https://sso.pdx.edu/cas/login"):
+    '''
+        @params:
+            username - the username to login with
+            password - the password to login with
+            outfile  - the file that the json will be written into.
+                       (defaults to "cookies.json")
+            url      - the url to login to.
+                       (defaults to "http://sso.pdx.edu/cas/login")
+    '''
     r = requests.get(url, auth=(username, password))
     cookies = r.cookies
     cookies_dict = cookies.get_dict()
